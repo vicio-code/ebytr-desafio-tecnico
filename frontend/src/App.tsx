f770bff6017081d7ee05d4490f88635677fa6035
lucca-falbo <luccafalbo@gmail.com>
2022-07-05 15:14:10 -0300

    basic frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TaskList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TaskList() {
+  return (
+    <div className='max-w-2xl max-h-96 flex justify-center border rounded h-screen w-screen'>
+        Blu
+    </div>
+  )
+}
